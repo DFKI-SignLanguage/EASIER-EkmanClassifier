1b22aac5ec26c459ca4cbcd9dbd0c76aa83b3a3e
Chirag Bhuvaneshwara <chiragbhuvaneshwar@gmail.com>
2021-08-11 20:00:44 +0200

    updated FePh dataset for cross entropy loss

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -4,11 +4,9 @@ from base import BaseDataLoader
 import os
 from torch.utils.data import Dataset
 import pandas as pd
-import numpy as np
-from sklearn.preprocessing import MultiLabelBinarizer
+# from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.utils import class_weight
 import numpy as np
-import cv2
 from PIL import Image
 
 
@@ -46,7 +44,7 @@ class FaceExpressionPhoenixDataset(Dataset):
         self.transform = transform
         self.target_transform = target_transform
 
-        mlb = MultiLabelBinarizer()
+        # mlb = MultiLabelBinarizer()
         y_df = pd.read_csv(self.y_csv_path)
         # Removing all data points with 'Face_not_visible' i.e no labels
         y_df.dropna(inplace=True)
@@ -57,7 +55,8 @@ class FaceExpressionPhoenixDataset(Dataset):
         y_df = y_df[y_df["num_labels"] == 1]
         self.image_inputs = y_df['External ID'].apply(lambda img_name: os.path.join(self.x_dir_path, img_name)).tolist()
 
-        self.labels = mlb.fit_transform(y_df['Facial_label'].to_numpy())
+        # self.labels = mlb.fit_transform(y_df['Facial_label'].to_numpy())
+        self.labels = y_df['Facial_label'].apply(lambda x: x[0]).to_numpy()
 
     def __len__(self):
         return len(self.image_inputs)
@@ -92,7 +91,8 @@ class FaceExpressionPhoenixDataset(Dataset):
 
     def get_sample_weights(self, idxs):
         print("Calculating sampler weights...")
-        labels_array = np.argmax(self.labels[idxs], axis=1)
+        # labels_array = np.argmax(self.labels[idxs], axis=1)
+        labels_array = self.labels[idxs]
         class_weights = class_weight.compute_class_weight(class_weight='balanced', classes=np.unique(labels_array),
                                                           y=labels_array)
         num_classes = len(self.label_names.keys())
