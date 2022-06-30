034d963c0688d0bf45c7da1c1dc7b47a8dc28104
Fabrizio Nunnari <fab.nunnari@gmail.com>
2022-06-30 13:04:55 +0200

    Labels normalization scripts now works with or without prediction columns.

--- Scripts/NormalizeLabels.py
+++ Scripts/NormalizeLabels.py
@@ -4,7 +4,9 @@ import pandas
 
 from data_loader.data_loaders import EASIER_CLASSES
 
-REQUIRED_COLUMNS = ["ImageName"] + EASIER_CLASSES + ["ClassName", "Class"]
+IMG_NAME_COL = ["ImageName"]
+LABEL_COLS = ["ClassName", "Class"]
+
 
 parser = argparse.ArgumentParser(description='Normalize the labels of a dataframe to the EASIER standard order.'
                                              'The source CSV must contain the following three columns: ImageName, ClassName, Class.')
@@ -50,12 +52,29 @@ print(dst_label_to_id)
 # Read the source map
 print("Loading source dataframe from {}".format(source_csv))
 source_df = pandas.read_csv(source_csv)
-
 print(source_df.head())
 
-# Prepare the output CSV
-dest_df = pandas.DataFrame(columns=REQUIRED_COLUMNS)
+for required_col in IMG_NAME_COL + LABEL_COLS:
+    if required_col not in source_df.columns:
+        raise Exception("No column {} in source dataframe.".format(IMG_NAME_COL))
 
+#
+# Check if the source dataset contains also predictions, or only the ClassName and Class columns
+if len(source_df.columns) > len(IMG_NAME_COL + LABEL_COLS):
+    print("Predictions detected: will rename/reorder also prediction columns")
+    output_columns = IMG_NAME_COL + EASIER_CLASSES + LABEL_COLS
+    sort_predictions = True
+else:
+    print("No prediction columns found.")
+    output_columns = IMG_NAME_COL + LABEL_COLS
+    sort_predictions = False
+
+#
+# Prepare the output CSV
+dest_df = pandas.DataFrame(columns=output_columns)
+
+#
+# Process dataframe rows
 for i, row in enumerate(source_df.itertuples()):
     # print(row)
     image_name = row.ImageName
@@ -72,22 +91,26 @@ for i, row in enumerate(source_df.itertuples()):
 
     dst_label_idx = dst_label_to_id[dst_label]
 
-    # Fill the prediction value columns
-    # Compose the labels dictionary. By default, values to n/a.
-    dst_row_dict = {label: float('nan') for label in EASIER_CLASSES}
-    for src_pred_lab, dst_pred_lab in src_label_to_label.items():
-        pred = getattr(row, src_pred_lab)
-        dst_row_dict[dst_pred_lab] = pred
-
     # Check consistency between label and idx
     if idx != src_label_to_id[label]:
         print(" WARNING!!! Line {}: For label '{}' there is a wrong corresponding ID. Expected {}, found {}.".format(i, label, src_label_to_id[label], idx))
 
-    dst_row_dict.update({
+    dst_row_dict = {
         "ImageName": image_name,
         "ClassName": dst_label,
         "Class": dst_label_idx
-    })
+    }
+
+    # If required, sort also the prediction columns
+    if sort_predictions:
+        # Fill the prediction value columns
+        # Compose the labels dictionary. By default, values to n/a.
+        predictions_dict = {label: float('nan') for label in EASIER_CLASSES}
+        for src_pred_lab, dst_pred_lab in src_label_to_label.items():
+            pred = getattr(row, src_pred_lab)
+            predictions_dict[dst_pred_lab] = pred
+
+        dst_row_dict.update(predictions_dict)
 
     dest_df = dest_df.append(dst_row_dict, ignore_index=True)
 
