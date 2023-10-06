bfeb4b944bb2e6e488c942869834dcb29a87c3a2
Fabrizio Nunnari <fab.nunnari@gmail.com>
2023-10-06 17:38:20 +0200

    Vectorized labels normalization. Test on 10k lines show improvement from 27 to 3.2 seconds.

--- Scripts/NormalizeLabels.py
+++ Scripts/NormalizeLabels.py
@@ -7,6 +7,50 @@ IMG_NAME_COL = ["ImageName"]
 LABEL_COLS = ["ClassName", "Class"]
 
 
+def _convert_single_line(row: pandas.Series, src_label_to_id, src_label_to_label, dst_label_to_id, sort_predictions) -> pandas.Series:
+    """Support method to vectorize the conversion of the lines.
+     Gets a line (as Seires as input) and returns the new line, again as Series."""
+
+    image_name = row.ImageName
+    label = row.ClassName
+    idx = row.Class
+
+    if label not in src_label_to_label:
+        raise Exception(f"Label '{label}' not mentioned in the conversion map.")
+
+    dst_label = src_label_to_label[label]
+
+    if dst_label not in dst_label_to_id:
+        raise Exception(f"Destination label '{dst_label}' not in the EASIER list")
+
+    dst_label_idx = dst_label_to_id[dst_label]
+
+    # Check consistency between label and idx
+    if idx != src_label_to_id[label]:
+        print(
+            f"WARNING!!! For label '{label}' there is a wrong corresponding ID. Expected {src_label_to_id[label]}, found {idx}.")
+
+    dst_row_dict = {
+        "ImageName": image_name,
+        "ClassName": dst_label,
+        "Class": dst_label_idx
+    }
+
+    # If required, sort also the prediction columns
+    if sort_predictions:
+        # Fill the prediction value columns
+        # Compose the labels dictionary. By default, values to n/a.
+        predictions_dict = {label: float('nan') for label in EASIER_CLASSES}
+        for src_pred_lab, dst_pred_lab in src_label_to_label.items():
+            pred = getattr(row, src_pred_lab)
+            predictions_dict[dst_pred_lab] = pred
+
+        dst_row_dict.update(predictions_dict)
+
+    dst_row = pandas.Series(dst_row_dict)
+    return dst_row
+
+
 def normalize_labels(input_df: pandas.DataFrame, normalization_map: list) -> pandas.DataFrame:
 
     src_label_to_id = dict()
@@ -35,7 +79,7 @@ def normalize_labels(input_df: pandas.DataFrame, normalization_map: list) -> pan
     #
     # Check if the source dataset contains also predictions, or only the ClassName and Class columns
     if len(input_df.columns) > len(IMG_NAME_COL + LABEL_COLS):
-        # print("Logit prediction columns found : will rename/reorder also prediction columns")
+        # print("Logit prediction columns found: will rename/reorder also prediction columns")
         output_columns = IMG_NAME_COL + EASIER_CLASSES + LABEL_COLS
         sort_predictions = True
     else:
@@ -44,49 +88,10 @@ def normalize_labels(input_df: pandas.DataFrame, normalization_map: list) -> pan
         sort_predictions = False
 
     #
-    # Prepare the output CSV
-    dest_df = pandas.DataFrame(columns=output_columns)
-
-    #
     # Process dataframe rows
-    for i, row in enumerate(input_df.itertuples()):
-        # print(row)
-        image_name = row.ImageName
-        label = row.ClassName
-        idx = row.Class
-
-        if label not in src_label_to_label:
-            raise Exception("Line {}: Label '{}' not mentioned in the conversion map.".format(i, label))
-
-        dst_label = src_label_to_label[label]
-
-        if dst_label not in dst_label_to_id:
-            raise Exception("Line {}: Destination label '{}' not in the EASIER list".format(i, dst_label))
-
-        dst_label_idx = dst_label_to_id[dst_label]
-
-        # Check consistency between label and idx
-        if idx != src_label_to_id[label]:
-            print(" WARNING!!! Line {}: For label '{}' there is a wrong corresponding ID. Expected {}, found {}.".format(i, label, src_label_to_id[label], idx))
-
-        dst_row_dict = {
-            "ImageName": image_name,
-            "ClassName": dst_label,
-            "Class": dst_label_idx
-        }
-
-        # If required, sort also the prediction columns
-        if sort_predictions:
-            # Fill the prediction value columns
-            # Compose the labels dictionary. By default, values to n/a.
-            predictions_dict = {label: float('nan') for label in EASIER_CLASSES}
-            for src_pred_lab, dst_pred_lab in src_label_to_label.items():
-                pred = getattr(row, src_pred_lab)
-                predictions_dict[dst_pred_lab] = pred
-
-            dst_row_dict.update(predictions_dict)
-
-        dest_df = dest_df.append(dst_row_dict, ignore_index=True)
+    normalized_df = input_df.apply(func=_convert_single_line, axis=1, args=(src_label_to_id, src_label_to_label, dst_label_to_id, sort_predictions))
+    # Convert columns in the wanted order
+    dest_df = normalized_df[output_columns]
 
     return dest_df
 
